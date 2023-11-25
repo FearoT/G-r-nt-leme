37399c5bb3b9801209e8b694701da3e3b896b13b
Fatih Enes Altınay <115661458+FearoT@users.noreply.github.com>
2023-11-25 19:43:43 +0300

    Add files via upload

---
+++ Ödev1/odev1.py
@@ -0,0 +1,18 @@
+import cv2
+import matplotlib.pyplot as plt
+
+foto = cv2.imread('tango.jpg')
+gri_foto = cv2.cvtColor(foto, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Tango", gri_foto)
+histogram = [0] * 256
+
+for satir in gri_foto:
+    for sutun in satir:
+        histogram[sutun] += 1
+
+plt.figure(figsize=(8, 5))
+plt.bar(range(256), histogram, color='red')
+plt.title('Görüntü Histogramı')
+plt.xlabel('Piksel Değeri')
+plt.ylabel('Piksel Sayısı')
+plt.show()
